93173bdaa1d5a6f1a69e5dee9ddb8eea158184da
Christopher <christopher.rocks@bell.net>
2017-06-15 18:05:29 -0400

    updated repo's hash

--- hash.py
+++ hash.py
@@ -1,9 +1,6 @@
 # please run all hashes through this file.
-# this may seem redundant now, but on the server, the proprietary hash algorithm is different. I promise
+# this may seem redundant now, but on the server, the proprietary hash algorithm is different. I promise.
 
 
 def hash_string(string):
     return string
-
-print 'sup'
-print 'dode'
